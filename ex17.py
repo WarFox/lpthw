22343a917638279d8e22f9ed0e31d3906a2510d0
Deepu Mohan Puthrote <github@deepumohan.com>
2014-10-12 17:00:00 +0530

    Exercise 17: Study Drills - Open and read file in single line

--- ex17.py
+++ ex17.py
@@ -6,9 +6,7 @@ script, from_file, to_file = argv
 
 print "Copying from %s to %s" % (from_file, to_file)
 
-# we could do this two lines in one line, how?
-in_file = open(from_file)
-indata = in_file.read()
+indata = open(from_file).read()
 
 print "The input file is %d bytes long" % len(indata)
 
@@ -22,4 +20,3 @@ out_file.write(indata)
 print "Alright, all done."
 
 out_file.close()
-in_file.close()
