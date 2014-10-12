e53432aced43b293acb8114eb97de464fbd3a3a6
Deepu Mohan Puthrote <github@deepumohan.com>
2014-10-12 17:08:53 +0530

    Exercise 17: Study Drills - Do everything in single line.

--- ex17.py
+++ ex17.py
@@ -1,22 +1,2 @@
 # Exercise 17: More Files
-from sys import argv
-from os.path import exists
-
-script, from_file, to_file = argv
-
-print "Copying from %s to %s" % (from_file, to_file)
-
-indata = open(from_file).read()
-
-print "The input file is %d bytes long" % len(indata)
-
-print "Does the output file exists? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
-
-out_file = open(to_file, "w")
-out_file.write(indata)
-
-print "Alright, all done."
-
-out_file.close()
+from sys import argv; from os.path import exists; script, from_file, to_file = argv; open(to_file, "w").write(open(from_file).read())
