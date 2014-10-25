e14515dcc4fd6eecfce2cf32bb05888a3707c657
Deepu Mohan Puthrote <github@deepumohan.com>
2014-10-25 21:09:22 +0100

    Excercise 33: Study Drills - Convert while loop to function

--- ex33.py
+++ ex33.py
@@ -1,16 +1,20 @@
 # Exercise 33: While Loops
-i = 0
-numbers = []
+def print_numbers(n):
+    i = 0
+    numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+    while i < n:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-    i = i + 1
-    print "Numbers now:", numbers
-    print "At the bottom i is %d", i
+        i = i + 1
+        print "Numbers now:", numbers
+        print "At the bottom i is %d", i
 
-print "The numbers: "
+    print "The numbers: "
 
-for num in numbers:
-    print num
+    for num in numbers:
+        print num
+
+print_numbers(6)
+print_numbers(2)
